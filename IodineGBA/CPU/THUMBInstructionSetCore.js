fbb563ff6b581d8164e5738627f0cd311dbd19f1
Grant Galitz <grantgalitz@gmail.com>
2013-02-15 18:18:05 -0500

    Changing some buffering settings

--- IodineGBA/CPU/THUMBInstructionSetCore.js
+++ IodineGBA/CPU/THUMBInstructionSetCore.js
@@ -1,4 +1,4 @@
-/* 
+/*
  * This file is part of IodineGBA
  *
  * Copyright (C) 2012 Grant Galitz
--- IodineGBA/GameBoyAdvanceEmulatorCore.js
+++ IodineGBA/GameBoyAdvanceEmulatorCore.js
@@ -24,8 +24,8 @@ function GameBoyAdvanceEmulator() {
 	this.faultFound = false;				//Did we run into a fatal error?
 	this.paused = true;						//Are we paused?
 	this.audioVolume = 1;					//Starting audio volume.
-	this.audioBufferUnderrunLimit = 15;		//Audio buffer minimum span amount over x interpreter iterations.
-	this.audioBufferSize = 30;				//Audio buffer maximum span amount over x interpreter iterations.
+	this.audioBufferUnderrunLimit = 25;		//Audio buffer minimum span amount over x interpreter iterations.
+	this.audioBufferSize = 50;				//Audio buffer maximum span amount over x interpreter iterations.
 	this.offscreenWidth = 240;				//Width of the GBA screen.
 	this.offscreenHeight = 160;				//Height of the GBA screen.
 	this.BIOS = [];							//Initialize BIOS as not existing.
